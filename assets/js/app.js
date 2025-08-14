611770ba09fb730b5a800a78ca500e615536a9b2
Christina Bakis <c.bakis@gmx.de>
2025-08-14 10:08:13 +0200

    feat: open a dialog for showing the image in big; add buttons for showing next and previous images and closing dialog

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 
 const imagesContainer = document.getElementById("imgDiv");
+const dialog = document.getElementById("dialog");
 
 let images = [
   "orangene Taglilie.jpg",
@@ -17,9 +18,51 @@ let images = [
 ];
 
 function displayImages() {
-  images.forEach((image) => {
+  images.forEach((image, index) => {
     imagesContainer.innerHTML += `
-      <img src="./assets/img/${image}" class="img-thumbnail" alt="${image.slice(0, -4)}" />
+      <img src="./assets/img/${image}" class="img-thumbnail" 
+      id="${index}"
+      onclick="openDialog(this)" alt="${image.slice(0, -4)}" />
     `;
   });
+}
+
+function openDialog(item) {
+  console.log(item.id, item);
+  createDialog(item);
+  dialog.showModal();
+}
+
+createDialog = (item) => {
+  dialog.innerHTML = "";
+  dialog.innerHTML += `
+      <button class="close" onclick="closeDialog()">&times;</button>
+      ${item.outerHTML}
+      <button class="next-image" id="prev" onclick="prevImage(${item.id})"><-</button>
+      <button class="next-image" id="next" onclick="nextImage(${item.id})">-></button>
+  `;
+}
+
+function closeDialog() {
+  dialog.close();
+  dialog.innerHTML = "";
+}
+
+// showing next and previous image onclick
+function nextImage(id) {
+let nextId = id + 1; 
+  if (id == images.length - 1) {
+  nextId = 0;
+  }
+  const nextImage = document.getElementById(nextId);
+createDialog(nextImage);
+}
+
+function prevImage(id) {
+let prevId = id - 1; 
+  if (id == 0) {
+  prevId = images.length - 1;
+  }
+  const prevImage = document.getElementById(prevId);
+createDialog(prevImage);
 }
